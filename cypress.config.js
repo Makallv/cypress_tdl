e436b22e655b0ba98eab6c49d03c5393bea624e9
Makal5 <marks.cimermanis@gmail.com>
2023-05-11 20:14:09 +0300

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: "zxg9gg",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
