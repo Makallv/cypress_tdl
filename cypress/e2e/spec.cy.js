1669a9e1bd557d4afc51bfe5da330f3db36d5933
Makal5 <marks.cimermanis@gmail.com>
2023-05-18 14:59:03 +0300

    Added logout case and shopping cart case

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,4 +1,5 @@
-import LoginPage from "../page-objects/Login.page.js";
+import LoginPage from "../page-objects/Login.page.js"
+import ProductPage from "../page-objects/Product.page";
 
 describe("Saucedemo", () => {
   beforeEach(() => {
@@ -27,5 +28,31 @@ describe("Saucedemo", () => {
     LoginPage.loginButton.should("be.visible");
     LoginPage.loginButton.click();
     LoginPage.errorMessage.should("have.text", "Epic sadface: Sorry, this user has been locked out.");
+  });
+
+  it("Scenario 4: Logout case", () => {
+    LoginPage.username.type("standard_user");
+    LoginPage.password.type("secret_sauce");
+    LoginPage.loginButton.should("be.visible");
+    LoginPage.loginButton.click();
+    ProductPage.sidebar.invoke('attr', 'aria-hidden').should('eq', 'true')
+    ProductPage.burgerMenu.click();
+    ProductPage.sidebar.invoke('attr', 'aria-hidden').should('eq', 'false')
+    ProductPage.logoutButton.click();
+    LoginPage.loginButton.should("be.visible");
+  });
+
+  it("Scenario 5: Add to cart case", () => {
+    LoginPage.username.type("standard_user");
+    LoginPage.password.type("secret_sauce");
+    LoginPage.loginButton.should("be.visible");
+    LoginPage.loginButton.click();
+    ProductPage.sauceLabBackpackAddCartButton.should("be.visible");
+    ProductPage.sauceLabBackpackAddCartButton.click();
+    ProductPage.sauceLabOnesieAddCartButton.should("be.visible");
+    ProductPage.sauceLabOnesieAddCartButton.click();
+    ProductPage.sauceLabTShirtAddCartButton.should("be.visible");
+    ProductPage.sauceLabTShirtAddCartButton.click();
+    ProductPage.shoppingCartBadge.should("have.text", "3");
   });
 });
---
+++ cypress/page-objects/Product.page.js
@@ -0,0 +1,30 @@
+class ProductPage {
+
+    static get logoutButton(){
+        return cy.get('#logout_sidebar_link');
+    }
+
+    static get burgerMenu(){
+        return cy.get('#react-burger-menu-btn');
+    }
+
+    static get sidebar(){
+        return cy.get('.bm-menu-wrap');
+    }
+
+    static get sauceLabBackpackAddCartButton() {
+        return cy.get('[data-test="add-to-cart-sauce-labs-backpack"]');
+    }
+    static get sauceLabTShirtAddCartButton() {
+        return cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]');
+    }
+    static get sauceLabOnesieAddCartButton() {
+        return cy.get('[data-test="add-to-cart-sauce-labs-onesie"]');
+    }
+    static get shoppingCartBadge() {
+        return cy.get(".shopping_cart_badge");
+    }
+
+}
+
+export default ProductPage;
